af0729dd194d1fe256d58edf20c5d8f9d0d0f3fb
mqkoh <30967444+mqkoh@users.noreply.github.com>
2018-07-01 00:37:11 +0800

    Edit memberId for Loan

--- src/main/scala/ch/makery/library/model/Loan.scala
+++ src/main/scala/ch/makery/library/model/Loan.scala
@@ -6,19 +6,19 @@ import scalikejdbc._
 
 import scala.util.Try
 
-class Loan (val loanIds: Int, val userIds: Int, val bookIds: Int) extends Database{
+class Loan (val loanIds: Int, val memberIds: Int, val bookIds: Int) extends Database{
 
   def this (){this((null).asInstanceOf[Int],(null).asInstanceOf[Int],(null).asInstanceOf[Int])}
   var loanId = IntegerProperty(loanIds)
-  var userId = IntegerProperty(userIds)
+  var memberId = IntegerProperty(memberIds)
   var bookId = IntegerProperty(bookIds)
 
   def save()  { //instance method
       Try(DB autoCommit { implicit session =>
         sql"""
-					insert into Loan (loadId, userId,
+					insert into Loan (loadId, memberId,
 						bookId) values
-						(${loanId.value}, ${userId.value}, ${bookId.value})
+						(${loanId.value}, ${memberId.value}, ${bookId.value})
 				""".update.apply()
       })
 
@@ -31,11 +31,11 @@ object Loan extends Database {
 
   def apply(
     loanId: Int,
-    userId: Int,
+    memberId: Int,
     bookId: Int
     ): Loan = new Loan(
     loanId,
-    userId,
+    memberId,
     bookId
     )
 
@@ -44,7 +44,7 @@ object Loan extends Database {
       sql"""
         create table Loan(
            loanId int not null GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
-           userId int not null,
+           memberId int not null,
            bookId int not null
         )
         """.execute.apply()
@@ -53,7 +53,7 @@ object Loan extends Database {
   def getAllLoans : List[Loan] = { // for select queries use "readOnly"
     DB readOnly { implicit session =>
       sql"select * from Loan".map(rs => Loan(rs.int("loanId"), // use map collection as select queries usually returns multiple values
-        rs.int("userId"),rs.int("bookId"))).list.apply()
+        rs.int("memberId"),rs.int("bookId"))).list.apply()
     } // return every row in database as a Loan
   }
 }
